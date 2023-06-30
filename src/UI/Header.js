2262a873b5c3d9d479b70b43b1b9c4647af55ff3
Mitali Padiya <mdpadiya10@gmail.com>
2023-06-30 10:28:15 +0530

    Added logout

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,25 +1,33 @@
 import { Link, useNavigate } from 'react-router-dom';
 import './Header.scss';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
+import { logout } from "../actions/auth";
 
 const Header = () => {
     const { isLoggedIn } = useSelector(state => state.auth);
     const { user } = useSelector(state => state.auth);
     const [selectedHeader, setSelectedHeader] = useState("module1");
+    const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const onProfileClick = () => {
         navigate("/profile");
     }
     const onHomeClick = () => {
         navigate("/");
     }
+    const onLogout = () => {
+        localStorage.clear();
+        setIsOpen(false);
+        dispatch(logout());
+    }
     return (
         <header>
             {isLoggedIn ? <div className='header-div'>
                 <div className='header-logo'>
-                    <img className="header-img" onClick={onHomeClick}/>
-                    <img className="catapult-img" onClick={onHomeClick}/>
+                    <img className="header-img" onClick={onHomeClick} />
+                    <img className="catapult-img" onClick={onHomeClick} />
                 </div>
 
                 <nav>
@@ -28,16 +36,25 @@ const Header = () => {
                     <Link className={selectedHeader == "module3" ? 'active' : ''} onClick={() => setSelectedHeader("module3")} to="/module3">Module 3</Link>
                 </nav>
                 <div className='user-div'>
-                    <div className='user-circle'  onClick={onProfileClick}>
+                    <div className='user-circle' onClick={onProfileClick}>
                         <img className="user-img" />
                     </div>
                     <span className='user-name'>{user && user.first_name && user.last_name ? user.first_name + " " + user.last_name : ""}</span>
-                    <img className="user-arrow" />
+                    <ul className="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+                        <li className={`nav-item dropdown ${isOpen ? 'show' : ''}`}>
+                            <a onClick={() => setIsOpen(prev => !prev)}>
+                                <img className="user-arrow" href="#" id="logout" data-toggle="dropdown" aria-haspopup="true" aria-expanded={isOpen ? true : false} />
+                            </a>
+                            <div className={`dropdown-menu dropdown-menu-right ${isOpen ? 'show' : ''}`} aria-labelledby="logout" onClick={onLogout}>
+                                Logout
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div> : <div className='header-logo'>
-                    <img className="header-img" onClick={onHomeClick}/>
-                    <img className="catapult-img" onClick={onHomeClick}/>
-                </div>}
+                <img className="header-img" onClick={onHomeClick} />
+                <img className="catapult-img" onClick={onHomeClick} />
+            </div>}
 
         </header>
     )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
