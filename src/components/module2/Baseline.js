c4ce9eb3b41eeadd058c6a864f4713262a89443e
ankit <ankitpawar0086@gmail.com>
2023-07-05 14:05:35 +0530

    calculated area UI

--- src/components/module2/Baseline.js
+++ src/components/module2/Baseline.js
@@ -2,50 +2,85 @@ import { useEffect, useState } from "react";
 import Form from "./Form";
 
 const Baseline = () => {
-    const [data, setData] = useState()
-    useEffect(() => {
-        setData({
-            "title": "Baseline scenario",
-            "subTitle": "Provident et aut veniam quia dolor dicta laboriosam pariatur nam quibusdam dicta beatae quas dolore.",
-            "inputs": [
-                {
-                    "heading": "Average annual electricity consumption",
-                    "subHeading": "Ut atque quia aut sunt. Vel quis quasi nostrum accusamus et vel",
-                    "value": "",
-                    "unit": "",
-                    "type": "input",
-                    "id": 1
-                },
-                {
-                    "heading": "",
-                    "subHeading": "",
-                    "value": "",
-                    "unit": "",
-                    "type": "input",
-                    "id": 1
-                },
-                {
-                    "heading": "",
-                    "subHeading": "",
-                    "value": "",
-                    "unit": "",
-                    "type": "input",
-                    "id": 1
-                }
-            ],
-            "calculated": [
-                {
-                    "heading": "",
-                    "value": "",
-                    "unit": "",
-                    "type": "calculated",
-                    "id": 1
-                }
-            ]
-        })
-    }, [])
-    return <div>
-        <Form data={data} />
+  const [data, setData] = useState();
+  useEffect(() => {
+    setData(
+      {
+          "key": "Baseline",
+          "data": {
+              "title": "Baseline scenario",
+              "subTitle": "Provident et aut veniam quia dolor dicta laboriosam pariatur nam quibusdam dicta beatae quas dolore.",
+              "data": [
+                  {
+                      "heading": "",
+                      "data": {
+                          "inputs": [
+                              {
+                                  "heading": "Average annual electricity consumption",
+                                  "subHeading": "Ut atque quia aut sunt. Vel quis quasi nostrum accusamus et vel",
+                                  "value": "",
+                                  "unit": "kWh",
+                                  "type": "input",
+                                  "id": 1
+                              },
+                              {
+                                  "heading": "Average annual gas consumption",
+                                  "subHeading": "Quis enim unde. Rerum corrupti voluptatum",
+                                  "value": "",
+                                  "unit": "kWh",
+                                  "type": "input",
+                                  "id": 2
+                              },
+                              {
+                                  "heading": "Emission factor for grid electricity",
+                                  "subHeading": "Et voluptatum harum. In rerum necessitatibus quis. Inventor",
+                                  "value": "",
+                                  "unit": "kgCO2e/kWh",
+                                  "type": "input",
+                                  "id": 3
+                              },
+                              {
+                                  "heading": "Emission factor for grid gas",
+                                  "subHeading": "Et voluptatum harum. In rerum necessitatibus quis. Inventor",
+                                  "value": "",
+                                  "unit": "kgCO2e/kWh",
+                                  "type": "input",
+                                  "id": 4
+                              }
+                          ],
+                          "calculated": [
+                              {
+                                  "heading": "Annual operational emissions for grid electricity",
+                                  "value": "",
+                                  "unit": "",
+                                  "type": "calculated",
+                                  "id": 1
+                              },
+                              {
+                                  "heading": "Annual operational emissions for grid gas",
+                                  "value": "",
+                                  "unit": "",
+                                  "type": "calculated",
+                                  "id": 2
+                              },
+                              {
+                                  "heading": "Total baseline emissions",
+                                  "value": "",
+                                  "unit": "",
+                                  "type": "calculated",
+                                  "id": 3
+                              }
+                          ]
+                      }
+                  }
+              ]
+          }
+      });
+  }, []);
+  return (
+    <div>
+      <Form data={data?.data} />
     </div>
-}
+  );
+};
 export default Baseline;
--- src/components/module2/Form.js
+++ src/components/module2/Form.js
@@ -2,26 +2,51 @@ import InputWithSideText from "../UI/InputWithSideText";
 import "./Form.css";
 
 const Form = ({ data }) => {
-    return <div>
-        <h2>{data?.title}</h2>
-        <h3>{data?.subTitle}</h3>
-        <div className="main">
-            <div className="form-input">
-                {data?.inputs.length ?
-                    <>{data?.inputs.map(input => {
-                        return <InputWithSideText />
-                    })}</>
-                    : null}
-            </div>
-            <div className="calculated-main">
-                {data?.calculated.length ? <>
-                    {data?.inputs.map(input => {
-                        return <div></div>
-                    })}
-                </> : null}
-            </div>
+  // console.log(data); //  console.log here to log the 'data' prop
 
+  return (
+    <div>
+      <h2>{data?.title}</h2>
+      <h3>{data?.subTitle}</h3>
+      <div className="main">
+        <div className="form-input">
+          {data?.data?.length ? (
+            <>
+              {
+                data?.data.map((group, index) => {
+                  {
+                    return group.data.inputs.length ? group?.data?.inputs.map((input, index) => {
+                      return <InputWithSideText value={input.value}
+                        unit={input.unit}
+                        type="number"
+                        placeholder="Enter value"
+                        heading={input.heading}
+                        subHeading={input.subHeading} />;
+                    }) : null
+                  }
+                })
+              }
+
+            </>
+          ) : null}
         </div>
+        <div className="calculated-main">
+          {/* {data?.calculated.length ? (
+            <>
+              {data?.inputs.map((input) => {
+                return <div></div>;
+              })}
+            </>
+          ) : null} */}
+          <h3 className="survey-para-2">Pending information</h3>
+          <p className="survey-heading-3">Annual operational emissions for grid electricity</p>
+          <h3 className="survey-para-2">Pending information</h3>
+          <p className="survey-heading-3">Annual operational emissions for grid gas</p>
+          <h3 className="survey-para-2">Pending information</h3>
+          <p className="survey-heading-3">Total baseline emissions</p>
+        </div>
+      </div>
     </div>
-}
+  );
+};
 export default Form;
