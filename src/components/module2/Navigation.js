33c7416f330d3748bcbb3cb1b402e1ce02f60c8d
Amit Sharma <amitsharma.satna@gmail.com>
2023-07-08 13:14:54 +0530

    navigation styles

--- src/components/module2/Navigation.js
+++ src/components/module2/Navigation.js
@@ -1,8 +1,26 @@
 import "./Navigation.css";
 const Navigation = () => {
-    return <div className="nav">
-        <div className="nav-item">SYSTEM SIZE & CAPITAL COST</div>
-        <div className="nav-item">BASELINE CONSUMPTION</div>
+    return <div className="c-nav">
+        <div className="inner-box">
+            <div>
+                <div className="c-nav-item current-item">GENERAL</div>
+                <div className="c-nav-item c-nav-subitem current-subitem">Baseline</div>
+                <div className="c-nav-item c-nav-subitem">Technologies</div>
+                <div className="c-nav-item c-nav-subitem">Economic parameters</div>
+            </div>
+            <hr />
+            <div className="c-nav-item">SYSTEM SIZE & CAPITAL COST</div>
+            <hr />
+            <div className="c-nav-item">SYSTEM SIZE & CAPITAL COST</div>
+            <hr />
+            <div className="c-nav-item">BASELINE CONSUMPTION</div>
+            <hr />
+            <div className="c-nav-item">EMISSION SAVINGS</div>
+            <hr />
+            <div className="c-nav-item">MARGINAL ABATEMENT COST CURVE (MACC)</div>
+            <hr />
+        </div>
+
     </div>
 }
 export default Navigation;
