fbbf8d2d4510c129ec470961d02cc2d7cd13d4d9
Mitali Padiya <mdpadiya10@gmail.com>
2023-08-08 11:46:53 +0530

    updated table

--- src/components/UI/Table.js
+++ src/components/UI/Table.js
@@ -1,31 +1,73 @@
 import Input from "./Input";
 import Result from "./Result";
 import Select from "./Select";
-import"./Result.css";
-import"./Table.css";
+import "./Result.css";
+import "./Table.css";
+import { useState } from "react";
 
 const Table = () => {
+
+  const [tableData, setTableData] = useState([]);
+  const [country, setCountry] = useState("India");
+  const [sector, setSector] = useState("");
+  const [subSector, setSubSector] = useState("");
+  const [cost, setCost] = useState("");
+  const [emission, setEmission] = useState("");
+  const [countries, setCountries] = useState(["India", "US"]);
+
+  const addEntry = () => {
+    const newEntry = {
+      id: tableData.length + 1,
+      country: country,
+      sector: sector,
+      subSector: subSector,
+      cost: cost,
+      emission: emission,
+    };
+    setTableData(prev => [...prev, newEntry]);
+  };
   return (
-    <table className="forms-table">
-      <thead>
-        <tr>
-          <th className="table-heading ">Country</th>
-          <th className="table-heading ">Sector</th>
-          <th className="table-heading ">Sub sector</th>
-          <th className="table-heading ">Cost</th>
-          <th className="table-heading ">Embodied emissions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><Select/></td>
-          <td><Select/></td>
-          <td><Select/></td>
-          <td><Input/></td>
-          <td><Result/></td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <table className="forms-table">
+        <thead>
+          <tr>
+            <th className="table-heading ">Country</th>
+            <th className="table-heading ">Sector</th>
+            <th className="table-heading ">Sub sector</th>
+            <th className="table-heading ">Cost</th>
+            <th className="table-heading ">Embodied emissions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            tableData.length > 0 ? tableData.map(data => {
+              return <tr>
+              <td>{data.country}</td>
+              <td>{data.sector}</td>
+              <td>{data.subSector}</td>
+              <td>{data.cost}</td>
+              <td>{data.emission}</td>
+            </tr>
+            }) : null
+          }
+          <tr>
+              <td><Select values={countries} value={country} onChange={(event)=>setCountry(event.target.value)}/></td>
+              <td><Select /></td>
+              <td><Select /></td>
+              <td><Input /></td>
+              <td><Result /></td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="finalresult-line">
+        <p className="add-entry" onClick={addEntry}>Add entry</p>
+        <div className="final-calculatedvalue">
+          <p>Total embodied emissions</p>
+          <Result className="result-value" />
+        </div>
+      </div>
+    </div>
+
   );
 }
 
--- src/components/module3/EmboidedEmisssions.js
+++ src/components/module3/EmboidedEmisssions.js
@@ -3,20 +3,6 @@ import Result from "../UI/Result";
 import Table from "../UI/Table";
 import "./EmboidedEmissions.css"
 const EmbodiedEmissions = () => {
-    const [tableData, setTableData] = useState([
-        { id: 1, col1: 'Country 1', col2: 'Sector 1', col3: 'SubSector 1', col4: 'Cost 1', col5: 'Emboided 1' },
-    ]);
-    const addEntry = () => {
-        const newEntry = {
-            id: tableData.length+1,
-            col1: `Country ${tableData.length + 1}`,
-            col2: `Sector ${tableData.length + 1}`,
-            col3: `SubSector ${tableData.length + 1}`,
-            col4: `Cost ${tableData.length + 1}`,
-            col5: `Emboided ${tableData.length + 1}`,
-        };
-        setTableData(prev=>[...prev, newEntry]);
-    };
     return <div>
         <div>
             <h2 className="form-heading">Embodied emissions calculator</h2>
@@ -25,13 +11,7 @@ const EmbodiedEmissions = () => {
             {/* <tr key={entry.id}> */}
                 <Table className="forms-table" />
                 {/* </tr> */}
-                <div className="finalresult-line">
-                    <p className="add-entry">Add entry</p>
-                    <div className="final-calculatedvalue">
-                        <p>Total embodied emissions</p>
-                        <Result className="result-value" />
-                    </div>
-                </div>
+                
             </div>
         </div>
 
